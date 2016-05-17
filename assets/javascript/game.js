20cbdc9c68ebc0cd7ea8f4a6cb9e2e1b6470dd48
Joey Reyes <jreyes88@gmail.com>
2016-05-17 12:24:41 -0500

    look at that character choice

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 
      /* Prepares Each Character Container */
      for (var i = 0; i < characterArray.length; i++) {
-          myHTML += "<div class='col-md-15 text-center' id=" + i + "><h3>" + characterArray[i].name + "</h3><div class='inner-img-container'><img class='hero-images' src=" + characterArray[i].img + "></div><p class='health'</p>" + "health: " + characterArray[i].healthPoints + "</div>";
+          myHTML += "<div class='col-md-15 text-center nullPane' id=" + i + "><h3>" + characterArray[i].name + "</h3><div class='inner-img-container'><img class='hero-images' src=" + characterArray[i].img + "></div><p class='health'</p>" + "health: " + characterArray[i].healthPoints + "</div>";
           console.log(i);
      };
 
@@ -16,8 +16,21 @@ $(document).ready(function() {
      $(".instructionPane").html(instructionHTML);
 
      /* User First Click */
-     $( ".activePane" ).click(function(event) {
-          console.log( "clicked: " + event.target.id );
+
+     var myHero = "";
+     var myVillain = "";
+
+     /* Hero Choice */
+     $( ".nullPane" ).click(function(event) {
+          if (myHero == "") {
+               var heroChoice = $(this);
+               console.log(heroChoice);
+               $(this).removeClass("nullPane").addClass("heroPane");
+               myHero = "full";
+               // $(".heroPane").append(heroChoice);
+          };
+          instructionHTML = "<h3>Now choose an enemy!!</h3>"
+          $(".instructionPane").html(instructionHTML);
      });
 
 })
