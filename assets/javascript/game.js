39d2e9d0e375f61678e0c2d43aafd7e7571689bc
Joey Reyes <jreyes88@gmail.com>
2016-05-18 17:59:25 -0500

    formatting is improved

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,10 +3,11 @@ $(document).ready(function() {
      /* Prepares Each Character Container */
      for (var i = 0; i < characterArray.length; i++) {
           myHTML += "<div class='col-md-15 text-center nullPane' id=" + i + "><h3>" + characterArray[i].name + "</h3><div class='inner-img-container'><img class='hero-images' src=" + characterArray[i].img + "></div><p class='health'</p>" + "health: " + characterArray[i].healthPoints + "</div>";
-          console.log(i);
+          //console.log(i);
      };
 
      instructionHTML = "<h3>Choose a character!</h3>";
+     nonFightPaneInstructions = "<h3>Characters Remaining:</h3>";
 
 
      /* This loads the data above onto the html */
@@ -14,11 +15,13 @@ $(document).ready(function() {
 
      /* Manipulates the Instruction Pane */
      $(".instructionPane").html(instructionHTML);
+     $(".nonFightPaneInstructions").html(nonFightPaneInstructions);
 
      /* User First Click */
 
      var myHero = "";
      var myVillain = "";
+     var clickCount = [];
 
      /* Hero Choice */
      $(".nullPane").click(function(event) {
@@ -26,6 +29,7 @@ $(document).ready(function() {
                var heroChoice = $(this);
                $(this).removeClass("nullPane").addClass("heroPane");
                myHero = "full";
+               clickCount++;;
           };
 
           /* Villain Choice */
@@ -37,17 +41,29 @@ $(document).ready(function() {
                     $(this).removeClass("nullPane").addClass("villainPane");
                     myVillain = "full";
 
+                    /* Moving Non-Chosen Characters */
+
                     if (myHero =="full" && myVillain == "full") {
                          $(".nullPane").parent().removeClass("activePane");
                          var nonChosenCharacters = $(".nullPane");
-                         console.log(nonChosenCharacters);
                          $(".nonFightPane").append(nonChosenCharacters);
+                         instructionHTML = "<h3>Fight!</h3>";
+                         $(".instructionPane").html(instructionHTML);
+                         $(".nonFightPaneInstructions").removeClass("fightInstructionsVisibility");
+                         clickCount++;
                     };
-
+                    console.log(clickCount);
+                    if (clickCount == 2) {
+                         gameHTML = "<div class='col-md-15 text-center heroPane' id=' + i + '><h3>' + characterArray[i].name + '</h3><div class='inner-img-container'><img class='hero-images' src=' + characterArray[i].img + '></div><p class='health'</p>' + 'health: ' + characterArray[i].healthPoints + '</div>";
+                         $(".activePane").html(gameHTML);
+                    };
                });
           };
      });
+     
 });
+
+
 
 myHTML = [];
 instructionHTML = [];
