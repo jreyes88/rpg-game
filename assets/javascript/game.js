992fb91e31d4fd410bf7102966e9aee91a678f16
Joey Reyes <jreyes88@gmail.com>
2016-05-20 10:04:52 -0500

    fight numbers are working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -26,22 +26,23 @@ $(document).ready(function() {
           if (myHero == "") {
                var heroChoice = $(this);
                $(this).removeClass("nullPane").addClass("heroPane");
-               myHero = "full";
-               console.log($(this));
+               myHero = $(this).attr("id");
+               console.log(myHero);
           };
 
           /* Villain Choice */
           instructionHTML = "<h3>Now choose an enemy!!</h3>"
           $(".instructionPane").html(instructionHTML);
 
-          if (myVillain == "" && myHero == "full") {
+          if (myVillain == "" && myHero !== -1) {
                $(".nullPane").click(function(event) {
                     $(this).removeClass("nullPane").addClass("villainPane");
-                    myVillain = "full";
+                    myVillain = $(this).attr("id");
+                    console.log(myVillain);
 
                     /* Moving Non-Chosen Characters */
 
-                    if (myHero =="full" && myVillain == "full") {
+                    if (myHero !== -1 && myVillain !== -1) {
                          $(".nullPane").parent().removeClass("activePane");
                          var nonChosenCharacters = $(".nullPane");
                          $(".nonFightPane").append(nonChosenCharacters);
@@ -51,9 +52,19 @@ $(document).ready(function() {
 
                          /* Create Fight Button */
                          $(".fightPane").append("<button id='fightButton'>Fight!</button>");
+
+                         /* Fighting */
                          $("#fightButton").click(function(event) {
-                              console.log("fight button clicked");
-                              var characterHealth = $( "li.item-1" )[ 0 ];
+
+                              /* Hero Attack */
+                              characterArray[myHero].attackPower = characterArray[myHero].attackPower + characterArray[myHero].originalAttackPower;
+                              characterArray[myVillain].healthPoints = characterArray[myVillain].healthPoints - characterArray[myHero].attackPower;
+                              console.log(characterArray[myVillain].healthPoints);
+
+                              characterArray[myHero].healthPoints = characterArray[myHero].healthPoints - characterArray[myVillain].counterAttackPower;
+                              $("div.heroPane > p.health").html("health: " + characterArray[myHero].healthPoints);
+                              // characterArray[myVillain].healthPoints = characterArray[myVillain].healthPoints - characterArray[myHero].counterAttackPower;
+                              $("div.villainPane > p.health").html("health: " + characterArray[myVillain].healthPoints);
                          });
                          // create element, add button, add paragraphs (could be living in there with display: none?)
 
@@ -77,38 +88,43 @@ var characterArray = [
      img: "assets/images/bob.png",
      // img1: "#", (.find for second image to change src for image tag)
      healthPoints: 100,
-     attackPower: 5,
+     attackPower: 0,
+     originalAttackPower: 5,
      counterAttackPower: 5
      },
 
      {
      name: "Linda",
      img: "assets/images/linda.png",
-     healthPoints: 110,
-     attackPower: 10,
-     counterAttackPower: 5
+     healthPoints: 105,
+     attackPower: 0,
+     originalAttackPower: 8,
+     counterAttackPower: 8
      },
      
      {
      name: "Tina",
      img: "assets/images/tina.png",
-     healthPoints: 120,
-     attackPower: 15,
-     counterAttackPower: 5
+     healthPoints: 110,
+     attackPower: 0,
+     originalAttackPower: 10,
+     counterAttackPower: 10
      },
 
      {
      name: "Gene",
      img: "assets/images/gene.png",
-     healthPoints: 125,
-     attackPower: 20,
-     counterAttackPower: 5
+     healthPoints: 115,
+     attackPower: 0,
+     originalAttackPower: 12,
+     counterAttackPower: 12
      },
 
      {
      name: "Louise",
      img: "assets/images/louise.png",
-     healthPoints: 130,
-     attackPower: 25,
-     counterAttackPower: 5
+     healthPoints: 120,
+     attackPower: 0,
+     originalAttackPower: 15,
+     counterAttackPower: 15
      }];
